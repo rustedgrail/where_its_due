ada7d8f93aa0e52dcdd795034d28f8854ce72be8
David Newell <beta@alumni.rice.edu>
2016-10-30 15:50:36 -0400

    Initial elm config

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,6 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+const elmDiv = document.querySelector('#elm-main');
+const elmApp = Elm.SeatSaver.embed(elmDiv);
